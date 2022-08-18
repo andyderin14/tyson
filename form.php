24cf5193fbb2f5f66b5e532a2e71b4bcc171d5eb
Andres Julian Derin <andresderin78@gmail.com>
2022-08-17 23:19:51 -0300

    Modificando pagina de prueab con detalles de colores esteticos, margenes, anchos, tipografÃias, etc

--- form.php
+++ form.php
@@ -2,22 +2,25 @@
 
 
 $nombre = $_POST ['nombre'];
+$email = $_POST ['email'];
 $mensaje = $_POST ['mensaje'];
+
 
 
 // Como me va a llegar a mi el cuerpo del mensaje al email //
 
 
-$mensaje = "Este mensaje fue enviado por el usuario: " . $nombre . "\r\n";
-$mensaje .= "Mensaje de usuario: " . $_POST ["mensaje"] . " \r\n";
-$mensaje .= "Enviado el: " . date ("d/m/Y" , time() );
+$referencia = "Este mensaje fue enviado por el usuario: " . $nombre . "\r\n";
+$referencia .= "Email del usuario: " . $email . "\r\n";
+$referencia .= "Mensaje de usuario: " . $_POST ["mensaje"] . "\r\n";
+$referencia .= "Enviado el: " . date ("d/m/Y" , time() );
 
 
 $destinatario = 'andresderin78@gmail.com'; // Email a donde va a llegar el mensaje //
-$asunto = 'Este email fue enviado desde la web de Kangre Burguerz'; // Asunto que me va a llegar al emaildesde la página //
+$asunto = 'Este mensaje fue enviado desde la web de Kangre Burguerz'; // Asunto que me va a llegar al emaildesde la página //
 
 
-mail($destinatario, $asunto , utf8_decode($mensaje) , $header);
+mail($destinatario, $asunto , utf8_decode($referencia) , $header);
 
 header('Location.exito.html');
 
