121c51d983146df70ddf7c2f6cde96e4dd5ed89c
Andres Julian Derin <andresderin78@gmail.com>
2022-08-20 15:43:38 -0300

    Arreglando formulario

--- form.php
+++ form.php
@@ -1,18 +1,19 @@
 <?php
 
 
-$nombre = $_POST ['nombre'];
+$nombre = $_POST ['name'];
+$apellido = $_POST ['lastname'];
 $email = $_POST ['email'];
-$mensaje = $_POST ['mensaje'];
+$mensaje = $_POST ['textarea'];
 
 
 
 // Como me va a llegar a mi el cuerpo del mensaje al email //
 
 
-$referencia = "Este mensaje fue enviado por el usuario: " . $nombre . "\r\n";
+$referencia = "Este mensaje fue enviado por el usuario: " . $nombre . $apellido "\r\n";
 $referencia .= "Email del usuario: " . $email . "\r\n";
-$referencia .= "Mensaje de usuario: " . $_POST ["mensaje"] . "\r\n";
+$referencia .= "Mensaje de usuario: " . $mensaje . "\r\n";
 $referencia .= "Enviado el: " . date ("d/m/Y" , time() );
 
 
@@ -20,9 +21,17 @@ $destinatario = 'andresderin78@gmail.com'; // Email a donde va a llegar el mensa
 $asunto = 'Este mensaje fue enviado desde la web de Kangre Burguerz'; // Asunto que me va a llegar al emaildesde la página //
 
 
-mail($destinatario, $asunto , utf8_decode($referencia) , $header);
 
-header('Location.exito.html');
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+          $nombre = $_POST ['name'];
+          .
+          $return = mail($destinatario, $asunto , utf8_decode($referencia));
+          }
 
+          if ($mailReturn == true) {
+                    echo "<div id = \"success\">E-mail enviado con éxito</div>";
+                    } else {
+                      echo "<div id = \"fail\">E-mail cannot be sended.</div>";
+                    }      
 
 ?>
