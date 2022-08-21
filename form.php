15c3f1e478d93bdc19f2829738d5094c4f682de9
Andres Julian Derin <andresderin78@gmail.com>
2022-08-21 01:10:34 -0300

    agregando carousel de bootstrap a la pagina de index

--- form.php
+++ form.php
@@ -1,3 +1,7 @@
+<h1>Titulo de la pagina</h1>
+
+
+
 <?php
 
 
@@ -11,27 +15,33 @@ $mensaje = $_POST ['textarea'];
 // Como me va a llegar a mi el cuerpo del mensaje al email //
 
 
-$referencia = "Este mensaje fue enviado por el usuario: " . $nombre . $apellido "\r\n";
-$referencia .= "Email del usuario: " . $email . "\r\n";
-$referencia .= "Mensaje de usuario: " . $mensaje . "\r\n";
-$referencia .= "Enviado el: " . date ("d/m/Y" , time() );
+$body = "Este mensaje fue enviado por el usuario: " . $nombre . $apellido "\r\n";
+$body .= "Email del usuario: " . $email . "\r\n";
+$body .= "Mensaje de usuario: "  . $mensaje . "\r\n";
+$body .= "Enviado el: " . date ("d/m/Y" , time() );
 
 
 $destinatario = 'andresderin78@gmail.com'; // Email a donde va a llegar el mensaje //
 $asunto = 'Este mensaje fue enviado desde la web de Kangre Burguerz'; // Asunto que me va a llegar al emaildesde la página //
+$header = 'Tyson';
+
+mail($destinatario, $asunto , utf8_decode($body) , $header);
+
+header('location:exito.html');
 
 
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-          $nombre = $_POST ['name'];
-          .
-          $return = mail($destinatario, $asunto , utf8_decode($referencia));
-          }
 
-          if ($mailReturn == true) {
-                    echo "<div id = \"success\">E-mail enviado con éxito</div>";
-                    } else {
-                      echo "<div id = \"fail\">E-mail cannot be sended.</div>";
-                    }      
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//           $nombre = $_POST ['name'];
+//           .
+//           $return = mail($destinatario, $asunto , utf8_decode($referencia));
+//           }
+
+//           if ($mailReturn == true) {
+//                     echo "<div id = \"success\">E-mail enviado con éxito</div>";
+//                     } else {
+//                       echo "<div id = \"fail\">E-mail cannot be sended.</div>";
+//                     }      
 
 ?>
